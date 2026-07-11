0a96ea0c0bd516d14de5750254b1fb04ccd664e5
colls111 <159204222+colls111@users.noreply.github.com>
2026-07-11 15:34:21 -0500

    Update card.js

--- js/components/card.js
+++ js/components/card.js
@@ -2,17 +2,29 @@ import { formatTime } from "../utils/format.js";
 
 export function Card(video) {
 
+    const progress = Number(localStorage.getItem(`progress-${video.id}`));
+    const percent = Math.min(progress / video.duration * 100, 100);
+
     return `
         <a class="card" href="/video/${video.id}" data-link>
 
             <div class="thumb">
+                <img src="${video.thumbnail}">
 
-                <img src="${video.thumbnail}" alt="${video.title}">
-
-                <span class="duration">
-                    ${formatTime(video.duration)}
-                </span>
-
+                ${
+                    showProgress
+                    ? `
+                    <div class="progress-bar">
+                        <div
+                            class="progress-fill"
+                            style="width:${percent}%">
+                        </div>
+                    </div>
+                    `
+                    : `
+                    <div class="duration">${formatTime(video.duration)}</div>
+                    `
+                }
             </div>
 
             <h3>${video.title}</h3>
