aa306100fc470c5d139ccc71f62996d5aa317768
colls111 <159204222+colls111@users.noreply.github.com>
2026-07-12 22:55:28 -0500

    Update card.js

--- js/components/card.js
+++ js/components/card.js
@@ -2,39 +2,54 @@ import { formatTime } from "../utils/format.js";
 
 export function Card(video, options = {}) {
 
-    const { showProgress = false } = options;
+    const details = [video.genre, video.subgenre]
+        .filter(Boolean)
+        .join(" • ");
 
     const progress = Number(localStorage.getItem(`progress-${video.id}`));
-    const percent = Math.min(progress / video.duration * 100, 100);
-
-    console.log(options);
-    console.log(showProgress);
+    const percent = video.duration
+        ? (progress / video.duration) * 100
+        : 0;
 
     return `
-        <a class="card" href="/video/${video.id}" data-link>
+        <a href="/video/${video.id}" class="card" data-link>
 
             <div class="thumb">
-                <img src="${video.thumbnail}">
+
+                <img
+                    src="${video.thumbnail}"
+                    alt="${video.title}"
+                    loading="lazy"
+                >
+
+                <div class="duration">
+                    ${formatTime(video.duration)}
+                </div>
 
                 ${
-                    showProgress
-                    ? `
-                    <div class="progress-bar">
-                        <div
-                            class="progress-fill"
-                            style="width:${percent}%">
-                        </div>
-                    </div>
-                    `
-                    : `
-                    <div class="duration">${formatTime(video.duration)}</div>
-                    `
+                    options.showProgress &&
+                    !isNaN(progress)
+                        ? `
+                            <div class="progress-bar">
+                                <div
+                                    class="progress-fill"
+                                    style="width:${percent}%"
+                                ></div>
+                            </div>
+                        `
+                        : ""
                 }
+
             </div>
 
             <h3>${video.title}</h3>
 
+            ${
+                options.showDetails && details
+                    ? `<p>${details}</p>`
+                    : ""
+            }
+
         </a>
     `;
-
 }
