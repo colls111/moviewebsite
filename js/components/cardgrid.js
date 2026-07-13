10807625bf5891856f8311c31d34cd89bc169b2f
colls111 <159204222+colls111@users.noreply.github.com>
2026-07-12 22:55:35 -0500

    Update cardgrid.js

--- js/components/cardgrid.js
+++ js/components/cardgrid.js
@@ -1,23 +1,18 @@
 import { Card } from "./card.js";
 
-export function CardGrid(title, videos) {
+export function CardGrid(title, videos, showDetails = false) {
 
     return `
-
         <section class="video-section">
 
-            <h2 class="section-title">
-                ${title}
-            </h2>
+            <h2 class="section-title">${title}</h2>
 
             <div class="grid">
-
-                ${videos.map(Card).join("")}
-
+                ${videos.map(video =>
+                    Card(video, { showDetails })
+                ).join("")}
             </div>
 
         </section>
-
     `;
-
 }
